81924f2b79f3e54a5549034cff22b1e9eb5aadee
erwin@gmail.com <erwinschleier@gmail.com>
2020-11-24 17:11:29 +0100

    Updates

---
+++ precache-manifest.069c3928e20816534b6ffe531b524e0e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "acaa97985026cc84fd4a",
+    "url": "/slot_machine/static/css/main.9f9f5262.chunk.css"
+  },
+  {
+    "revision": "acaa97985026cc84fd4a",
+    "url": "/slot_machine/static/js/main.451a276c.chunk.js"
+  },
+  {
+    "revision": "31cd68113d752c53ece5",
+    "url": "/slot_machine/static/js/runtime~main.3ad10eea.js"
+  },
+  {
+    "revision": "e297d0c18bf0b643c29b",
+    "url": "/slot_machine/static/js/2.5f18798a.chunk.js"
+  },
+  {
+    "revision": "d77cb1779a8aeb0701ad5841d064a369",
+    "url": "/slot_machine/static/media/strawberry.d77cb177.png"
+  },
+  {
+    "revision": "abf2234f9a3923581ee469a9fdacc34d",
+    "url": "/slot_machine/static/media/banana.abf2234f.png"
+  },
+  {
+    "revision": "7b3e3f1cf53fb9774f28665ce2fd60d3",
+    "url": "/slot_machine/static/media/orange.7b3e3f1c.png"
+  },
+  {
+    "revision": "0208eeabaf464ef48734afc7213356f0",
+    "url": "/slot_machine/static/media/monkey.0208eeab.png"
+  },
+  {
+    "revision": "5419a33c5eeffab8be06e5635449b00c",
+    "url": "/slot_machine/static/media/win.5419a33c.wav"
+  },
+  {
+    "revision": "05c73073813b23e27dfc90b2f42fb5d1",
+    "url": "/slot_machine/static/media/fail.05c73073.wav"
+  },
+  {
+    "revision": "e0f2b0be83f7728e712e9e489a707b07",
+    "url": "/slot_machine/static/media/coin.e0f2b0be.wav"
+  },
+  {
+    "revision": "44f8afc7e399abb002625f4d6e0bed2a",
+    "url": "/slot_machine/index.html"
+  }
+];
